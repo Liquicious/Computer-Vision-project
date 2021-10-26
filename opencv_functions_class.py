bdade89922ba066f61438e675d3e4904698c5739
Liquicious <45201805+Liquicious@users.noreply.github.com>
2021-10-27 01:02:00 +0400

    Add files via upload

---
+++ opencv_functions_class.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+
+
+class OpencvImage:
+    def __init__(self, image_file):
+        self.image = image_file
+        self.border = None
+
+    def show(self):
+        cv2.imshow('0', self.image)
+        cv2.waitKey(0)
+
+    def load(self):
+        self.image = cv2.imread(self.image)
+
+    def normalize(self):
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        _, img_bin = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY_INV)
+        coords = np.column_stack(np.where(img_bin == 255))
+        angle = 90 - cv2.minAreaRect(coords)[-1]
+        (h, w) = self.image.shape[:2]
+        center = (w // 2, h // 2)
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated = cv2.warpAffine(self.image, rotation_matrix, (w, h), flags=cv2.INTER_CUBIC,
+                                 borderMode=cv2.BORDER_REPLICATE)
+        self.image = rotated
+
+    def extract_lines(self):
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        _, img_bin = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY_INV)
+
+        structuring_element = np.ones((1, 50), np.uint8)
+        erode_image = cv2.erode(img_bin, structuring_element, iterations=1)
+        hor_dilate_image = cv2.dilate(erode_image, structuring_element, iterations=1)
+
+        structuring_element = np.ones((50, 1), np.uint8)
+        erode_image = cv2.erode(img_bin, structuring_element, iterations=1)
+        ver_dilate_image = cv2.dilate(erode_image, structuring_element, iterations=1)
+        return hor_dilate_image,  ver_dilate_image
+
+    def merge_lines(self, horizontal_lines, vertical_lines):
+        structuring_element = np.ones((3, 3), np.uint8)
+        merge_image = horizontal_lines + vertical_lines
+        merge_image = cv2.dilate(merge_image, structuring_element, iterations=2)
+        self.border = merge_image
+
+
+img = OpencvImage("res/csv.jpg")
+
+img.load()
+img.show()
+
+img.normalize()
+img.show()
+
+hor, ver = img.extract_lines()
+img.merge_lines(hor, ver)
+
+cv2.imshow('0', img.border)
+cv2.waitKey(0)
